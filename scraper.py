bf4507797dbc9cadbb40279662fbd0c97768d7c2
Adam Owada <adamowada@gmail.com>
2023-03-13 13:08:40 -0700

    Added async functions for parsing Zoom links from a course syllabus.

--- scraper.py
+++ scraper.py
@@ -4,8 +4,28 @@ from playwright.async_api import async_playwright
 import re
 
 
-if __name__ == "__main__":
+async def parse_zoom_links(playwright, syllabus):
+    chromium = playwright.chromium  # or "firefox" or "webkit".
+    browser = await chromium.launch()
+    page = await browser.new_page()
+
+    # Get page content
+    await page.goto(syllabus)
+    content = await page.content()
+
+    return content
+
+
+async def main():
     print("What is the course syllabus link?")
     print("- For example: 'https://canvas.instructure.com/courses/5755799/assignments/syllabus'")
     syllabus = input("> ")
     print(syllabus)
+    async with async_playwright() as playwright:
+        #
+        content = await parse_zoom_links(playwright, syllabus)
+        print(content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
